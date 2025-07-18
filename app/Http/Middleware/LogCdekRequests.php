9f62af479201eb6f796319c278815921b022cfd1
screwthecap1 <chanskiy777@mail.ru>
2025-07-18 13:28:15 +0400

    Log Request Update

--- app/Http/Middleware/LogCdekRequests.php
+++ app/Http/Middleware/LogCdekRequests.php
@@ -12,17 +12,17 @@ class LogCdekRequests
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
      */
     public function handle(Request $request, Closure $next): Response
     {
         if (str_contains($request->path(), 'pvz')) {
-            Log::info('[CDEK] Incoming request', [
-                'method' => $request->method(),
-                'path' => $request->path(),
-                'params' => $request->all(),
-                'ip' => $request->ip(),
-            ]);
+            Log::info('[CDEK] Incoming request: ' . json_encode([
+                    'method' => $request->method(),
+                    'path' => $request->path(),
+                    'params' => $request->all(),
+                    'ip' => $request->ip(),
+                ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
         }
         return $next($request);
     }
